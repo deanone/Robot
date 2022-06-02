2404b66dd1a932ffcbb61f904d1d2ab32142b735
deanone <athanasios.salamanis@gmail.com>
2022-06-02 14:06:40 +0300

    1. Added docstrings. 2. Tested (manually) the functionality. TODO: unit tests.

--- Robot.py
+++ Robot.py
@@ -1,18 +1,27 @@
-'''
-The class implementing a robot moving in an infinite 2-dimensional space.
-'''
 class Robot:
-	def __init__(self):
-		self.row_pos = 0
-		self.col_pos = 0
-		heading = 0
-
-	def __init__(self, row_pos, col_pos, heading):
+	''' A class implementing a robot moving in an infinite 2D space. '''
+	def __init__(self, row_pos = 0, col_pos = 0, heading = 0):
+		'''
+		Parametrized constructor.
+		
+		Parameters:
+		row_pos (int): the row position of the robot
+		col_pos (int): the column position of the robot
+		heading (int): the heading of the robot. It can only take four values [0, 1, 2, 3] that correspond to the directions [south, west, north, east]
+		
+		'''
 		self.row_pos = row_pos
 		self.col_pos = col_pos
 		self.heading = heading
 
-	def turn(turn_instruction):
+	def turn(self, turn_instruction):
+		'''
+		Robot turn right or left.
+
+		Parameters:
+		turn_instruction (str): the turn instruction coming from the end user. It can either be 'right' or 'left'
+		
+		'''
 		if turn_instruction == 'right':
 			self.heading += 1
 			if self.heading == 4:
@@ -24,8 +33,15 @@ class Robot:
 		else:
 			pass
 
-	def move(move_instruction):
-		if move_instruction == 'front':
+	def move(self, move_instruction):
+		'''
+		Robot move forward or backward.
+
+		Parameters:
+		move_instruction (str): the move instruction coming from the end user. It can either be 'forward' or 'backward'
+		
+		'''
+		if move_instruction == 'forward':
 			if self.heading == 0:
 				self.row_pos += 1
 			elif self.heading == 1:
@@ -36,7 +52,7 @@ class Robot:
 				self.col_pos += 1
 			else:
 				pass
-		elif move_instruction == 'back':
+		elif move_instruction == 'backward':
 			if self.heading == 0:
 				self.row_pos -= 1
 			elif self.heading == 1:
@@ -50,8 +66,12 @@ class Robot:
 		else:
 			pass
 
-	def print_pos():
+	def print(self):
+		'''
+		Prints the current position of the robot.
+
+		'''
 		heading_directions = {0:'south', 1:'west', 2:'north', 3:'east'}
-		print('My current row position is {row_pos}'.format(row_pos = self.row_pos))
-		print('My current column position is {col_pos}'.format(col_pos = self.col_pos))
-		print('My current heading is {heading}'.format(heading = heading_directions[self.heading]))
+		print('------ Current Position ------')
+		print('({row_pos}, {col_pos}, {heading})'.format(row_pos = self.row_pos, col_pos = self.col_pos, heading = heading_directions[self.heading]))
+		print()
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-'''
-The starting point of the Robot application.
-'''
 from Robot import Robot
 
 
 def main():
+	''' The starting point of the Robot application. '''
+	robot = Robot()
 	while True:
-		robot = Robot()
-		instruction = input('What to do? Type \'move\' for moving and \'turn\' for turning\n')
-		if instruction == 'move':
-			move_instruction = input('Forward or backward? Type \'front\' for forward and \'back\' for backward\n')
-			robot.move(move_instruction)
-		elif instruction == 'turn':
-			turn_instruction = input('Right or left? Type \'right\' for right and \'left\' for left\n')
-			robot.turn(turn_instruction)
-		else:
-			print('Type \'move\' for moving and \'turn\' for turning')
+		try:
+			instruction = input('What to do? Type \'move\' for moving, \'turn\' for turning and \'print\' for printing the current position of the robot.\n')
+			print()
+			
+			# verifying the input instruction will either be 'move' or 'turn'
+			while instruction != 'move' and instruction != 'turn' and instruction != 'print':
+				instruction = input('Wrong instruction. Type \'move\' for moving, \'turn\' for turning and \'print\' for printing the current position of the robot.\n')
+			
+			if instruction == 'move':
+				move_instruction = input('Forward or backward? Type \'forward\' for forward and \'backward\' for backward\n')
+
+				# verifying the move instruction is either 'forward' or 'backward'
+				while move_instruction != 'forward' and move_instruction != 'backward':
+					move_instruction = input('Wrong move instruction. Type \'forward\' for forward and \'backward\' for backward\n')
+
+				robot.move(move_instruction)
+				print()
+
+			elif instruction == 'turn':
+				turn_instruction = input('Right or left? Type \'right\' for right and \'left\' for left\n')
+
+				# verifying the turn instruction is either 'right' or 'left'
+				while turn_instruction != 'right' and turn_instruction != 'left':
+					turn_instruction = input('Wrong turn instruction. Type \'right\' for right and \'left\' for left\n')
+
+				robot.turn(turn_instruction)
+				print()
+
+			elif instruction == 'print':
+				robot.print() 
+
+			else:
+				pass
+		except KeyboardInterrupt:
+			break
+
+	print('\nGoodbye!')
 
 
 if __name__ == '__main__':
